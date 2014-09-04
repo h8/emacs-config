0fee6399297a42d4f18802e84613e5df2d0b3b15
ds <dmitry.stropaloff@gmail.com>
2014-09-04 19:49:25 +0300

    auto indent

--- edit.d/visual.el
+++ edit.d/visual.el
@@ -2,4 +2,13 @@
 (tool-bar-mode -1)
 
 ;; Enable tab bar
-(tabbar-mode)
+(tabbar-mode t)
+(global-set-key [C-tab] 
+		(lambda () 
+		  (interactive)
+		  (if tabbar-mode 
+		      (tabbar-forward) 
+		    (tabbar-mode t))))
+
+;; Auto indent
+(define-key global-map (kbd "RET") 'newline-and-indent)
