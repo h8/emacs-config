4292333df84f90d8a58d02211898d7528f0be821
ds <dmitry.stropaloff@gmail.com>
2014-09-05 18:43:50 +0300

    scala mode fix and duplicate line addon

--- edit.d/addons.el
+++ edit.d/addons.el
@@ -1,2 +1,13 @@
 ;; Move lines, words and regions with Alt+Arrows
 (drag-stuff-global-mode)
+
+;; Duplicate line
+(defun duplicate-line()
+  (interactive)
+  (move-beginning-of-line 1)
+  (kill-line)
+  (yank)
+  (open-line 1)
+  (next-line 1)
+  (yank))
+(global-set-key (kbd "C-d") 'duplicate-line)
