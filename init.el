d6210a44bacd5265961c9ba5bf4e347b5347a30c
Dmitry Stropaloff <dmitry.stropaloff@gmail.com>
2015-11-28 15:39:02 +0100

    Added some options for CIDER and Clojure modes.

--- init.el
+++ init.el
@@ -144,3 +144,28 @@
 (add-hook 'cider-repl-mode-hook #'company-mode)
 (add-hook 'cider-mode-hook #'company-mode)
 
+(put 'erase-buffer 'disabled nil)
+
+(require 'clojure-mode)
+
+(define-clojure-indent
+  (defroutes 'defun)
+  (GET 2)
+  (POST 2)
+  (PUT 2)
+  (DELETE 2)
+  (HEAD 2)
+  (ANY 2)
+  (context 2))
+
+;; CIDER configuration
+
+(setq cider-repl-pop-to-buffer-on-connect nil)
+(setq cider-popup-stacktraces nil)
+(setq cider-repl-popup-stacktraces t)
+(setq cider-auto-select-error-buffer t)
+(setq nrepl-buffer-name-separator "-")
+(setq nrepl-buffer-name-show-port t)
+
+(provide 'init)
+;;; init.el ends here
